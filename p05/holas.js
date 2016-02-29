3efe32858d2740582951d8d7f44fe6cc61fac8be
Cristobal Dominguez <cristobald@me.com>
2016-02-29 14:49:17 -0300

    Corrección Pregunta 05

--- p05/holas.js
+++ p05/holas.js
@@ -1,10 +1,14 @@
 $(document).ready(function() {
-	var q = parseInt(prompt("Escriba la cantidad de holas"));
-	if (q > 0) {
+	var q = parseInt(prompt("Escriba los segundos de espera"));
+	/*if (q > 0) {
 		for(var i = 1; i <= q; i++){
 			$("body").append("hola<br />");
 		};
 	} else {
 		$("body").append("El número escrito no es válido");
-	}
+	}*/
+
+	var milisegundos = q * 1000;
+
+	setTimeout(function(){ $("body").append("hola<br />"); }, milisegundos);
 });
